92244eaea047f601fdedb4d4d951dab9c512a944
Dan Lyons <danielclyons@yahoo.com>
2020-03-26 17:14:56 -0700

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 // Assignment code here
+// var password_length = 0;
+var generatePassword = function() {
+var count = 0;
+  window.alert("Please answer prompts for password criteria.");
+  var password_length = window.prompt("How many characters would you like your password to be? Please choose between 8 and 128 characters.");
+  password_length = parseInt (password_length);
+  var a = Number.isInteger(password_length);
+
+  if ((a)&&(password_length>=8 && password_length<=128)) {
+    
+  } else {
+    window.alert("Invalid Entry! Programm Quitting!");
+   return;
+  }
+
+
+}
 
 
 // Get references to the #generate element
