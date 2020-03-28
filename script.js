824deb435e6430451de28c82a00415ba27894a85
Dan Lyons <danielclyons@yahoo.com>
2020-03-28 11:51:22 -0700

    third commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Assignment code here
 // var password_length = 0;
 var generatePassword = function() {
-var count = 0;
   window.alert("Please answer prompts for password criteria.");
   var password_length = window.prompt("How many characters would you like your password to be? Please choose between 8 and 128 characters.");
 
@@ -19,48 +18,54 @@ var count = 0;
     window.alert("Invalid Entry! Programm Quitting!");
    return;
   }
+  window.alert("password length is " + password_length);
+  var letters_lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var letters_capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var special_chars = "~!@#$%^&";
+  var numbers = "0123456789";
+  var string_all_options = "";
 
-  var count=0;  
+  window.alert("password length = " + password_length);
+  var count = 0;
+
 
   var  lower_case =  window.confirm("Incldue lower case letters?");
   window.alert("Add lower case letters?: " + lower_case);
   if (lower_case){
-    count=count+1
-    var extra_flag = lower_case;
+    string_all_options = string_all_options.concat(letters_lowercase);
+    count = count + 1;
   }
   var  upper_case = window.confirm("Incldue upper case letters?");
   window.alert("Add upper case letters?: " + upper_case);
   if (upper_case){
-    count=count+1;
-    var extra_flag = upper_case;
+    string_all_options = string_all_options.concat(letters_capital);
+    count = count + 1;
+
   }
   var numeric = window.confirm("Incldue numeric characters?");
   window.alert("Add numeric characters?: " + numeric);
   if (numeric){
-    count=count+1;
-    var extra_flag = numeric;
+    string_all_options = string_all_options.concat(numbers);
+    count = count + 1;
   }
   var special_character = window.confirm("Incldue special characters?");
   window.alert("Add special characters?: " + special_character);
   if (special_character){
-    count=count+1;
-    var extra_flag = special_character;
+    string_all_options = string_all_options.concat(special_chars);
+    count = count + 1;
   }
-  window.alert("count = " + count);
   
-  var letters = "abcdefghijklmnopqrstuvwxyz";
-  var special_chars = "~!@#$%^&";
   var divisions = password_length/count;
   divisions = Math.floor(divisions);
-  window.alert("divisions = " + divisions);
   
+
   var password = "";
 
 if (lower_case){
  for (var i = 0; i < divisions; i++){
- var random_char = Math.floor(Math.random()*letters.length)+1;
+ var random_char = Math.floor(Math.random()*letters_lowercase.length)+1;
  window.alert("random char is :" + random_char);
- var char = letters.charAt(random_char);
+ var char = letters_lowercase.charAt(random_char);
  window.alert("char is :" + char);
   password = password.concat(char); 
 }
@@ -68,9 +73,9 @@ if (lower_case){
 
 if (upper_case){
 for (var i = 0; i < divisions; i++){
-  var random_char =Math.floor(Math.random()*letters.length)+1;
-  var char = letters.charAt(random_char);
-  var char = char.toUpperCase();
+  var random_char =Math.floor(Math.random()*letters_capital.length)+1;
+  var char = letters_capital.charAt(random_char);
+  // var char = char.toUpperCase();
   password = password.concat(char);
 }
 }
@@ -90,17 +95,21 @@ if (special_character) {
   } 
 }
  var extra_chars = password_length - password.length;
+ window.alert("extra chars is " + extra_chars + " password is " + password);
 
  if (extra_chars > 0) {
   for (var i = 0; i < extra_chars; i++){
-    var random_char =Math.floor(Math.random()*extra_flag.length)+1;
-    window.alert("extra flag length is : " + extra_flag.length);
+    var random_char =Math.floor(Math.random()*string_all_options.length)+1;
+    var char = string_all_options.charAt(random_char);
+    // window.alert("extra flag length is : " + extra_flag.length);
     // var char = extra_flag.charAt(random_char);
-    // password = password.concat(char);
+    password = password.concat(char);
+    // window.alert("string all options is " + string_all_options);
   } 
 }
 
   window.alert("password is " + password);
+  return password ;
 }
 
 
